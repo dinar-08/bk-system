84d3282c65e93c1fb4d943db22f32347aaae8866
Dinar Kurniawan <220101087@mhs.udb.ac.id>
2026-08-26 07:25:05 +0700

    Add proxy trust and Google verification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => RoleMiddleware::class,
             'must.change.password' => MustChangePassword::class,
